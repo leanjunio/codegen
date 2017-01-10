2fd3d82a9f8cfd861c4fb2473fd8907bc702a0c6
Lean Junio <leanjunioo@gmail.com>
2017-01-10 10:55:25 -0500

    added a twitter button

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+var twitterQuote = '';
+
 $(document).ready(function() {
 
 	// Get quotes on load...
    newQuote();
+   twitterQuote = 'empty';
 
    // As well as when the button is clicked...
    $('.new-quote').click(function()	{
    	newQuote();
    })
 
+   $('.tweet').click(function() {
+      window.open("https://twitter.com/intent/tweet?text=" + twitterQuote + ' App by @leanjunio');
+      //console.log(twitterQuote);
+   })
 });
 
 function newQuote(quote) {
@@ -18,7 +25,13 @@ function newQuote(quote) {
     	url: 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?',
     	success: function(data)	{
     		$('.quote-container').text(data.quoteText);
-    		$('.author-container').text(data.quoteAuthor);
+
+        if(!data.quoteAuthor) {
+          $('.author-container').text('Anonymous');
+        } else  {
+          $('.author-container').text(data.quoteAuthor);
+        }
+        twitterQuote = data.quoteText + ' by: ' + data.quoteAuthor;
     	}
     });
 }
